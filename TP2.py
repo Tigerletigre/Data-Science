db64c2b02d7791ab0948a491f4bef34f5a3c075a
Louis Percher <louis.percher@gmail.com>
2026-09-23 16:00:43 +0200

    Depot tp2 Python bis

--- TP2.py
+++ TP2.py
@@ -102,5 +102,76 @@ A.ajouter_salle(capacite=20)
 B = Batiment('Beta')
 B.ajouter_salle(10)   
 
+#Exercice 2
+
+class Adherent:
+    def __init__(self,discipline:str,nom:str,email:str,tel:str,nom_urgence:str,email_urgence:str,tel_urgence:str)->None:
+        cpt = 0
+        for i in email:
+            if i == '@' :
+                cpt+=1
+        if cpt != 1:
+            raise ValueError ('Votre adresse mail doit contenir un seul @')
+        if len(tel)!=10 or not tel.isdigit():
+            raise ValueError('Votre numero de telephone doit contenir 10 chiffre')
+        cpt=0    
+        for i in email_urgence:
+            if i == '@' :
+                cpt+=1
+        if cpt != 1:
+            raise ValueError ('Votre adresse mail urgence doit contenir un seul @')
+        if len(tel_urgence)!=10 or not tel_urgence.isdigit():
+            raise ValueError('Votre numero de telephone urgence doit contenir 10 chiffre')
+  
+        self._discipline:str = discipline
+        self._nom:str = nom
+        self._email:str = email
+        self._tel:str= tel
+        self._nom_urgence:str = nom_urgence
+        self._email_urgence:str = email_urgence
+        self._tel_urgence:str= tel_urgence
+        
+    def coordonnees(self)->str:
+        return f'nom:{self._nom} ,email:{self._email},tel:{self._tel}'
+    def coordonnees_urgence(self)->str:
+        return f'nom_urgence:{self._nom_urgence} ,email_urgence:{self._email_urgence},tel_urgence:{self._tel_urgence}'
    
-    
+a = Adherent('badminton','Louis','lo@g','0123456789','raphael','po@m','9876543210')
+a.coordonnees
+class entraineur:
+    def __init__(self,nom:str,email:str,tel:int,discipline:str,jour_presence:str,heure_debut:int,heure_fin:int)
+        for i in email:
+            if i == '@' :
+                cpt+=1
+        if cpt != 1:
+            raise ValueError ('Votre adresse mail doit contenir un seul @')
+        if len(tel)!=10 or not tel.isdigit():
+            raise ValueError('Votre numero de telephone doit contenir 10 chiffre')    
+        
+        self._nom:str = nom
+        self._email:str = email
+        self._tel:str= tel
+        self._discipline:str = discipline
+        semaine = ['lundi','mardi','mercredi','jeudi','vendredi']
+        for i in semaine:
+            if self._jour_presence != semaine[i]:
+                raise ValueError ('Choisir un jour entre lundi et vendredi')
+        self._jour_presence:str = jour_presence
+        if self._heure_debut > self._heure_fin or self._heure_debut < 10 or  self._heure_fin > 18:
+            raise ValueError('Choisir un créneau entre 10h et 18h')
+        self._heure_debut:int = heure_debut
+        self._heure_fin:int = heure_fin       
+        
+        
+        def coordonnees(self)->str:
+            return f'nom:{self._nom} ,email:{self._email},tel:{self._tel}'
+
+class Discipline:
+    def __init__(self,nom:str,capacite:int)->None:
+        self._nom:str = nom
+        self._capacite:int = int
+        self._creneaux:list= []
+        
+    def ajouter_creneau(self,jour:str,heure_debut:int,heure_fin:int)->None:
+        
+
